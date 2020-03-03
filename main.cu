b93725fe3271459bf98a46954d2016bb255c49c5
chilipepperhott <sirius.potter522@gmail.com>
2020-03-03 08:32:14 -0700

    Now running with a thread to each pixel up to 500x500 (tested)

--- main.cu
+++ main.cu
@@ -5,41 +5,36 @@
 const int MAX_ITERATION = 255;
 
 __global__
-void cudaCalc(int w, int h, double mx, double my, double zoom, float *results){
-  int index = threadIdx.x;
-  int stride = blockDim.x;
+void cudaCalc(int w, int h, double mx, double my, double zoom, double *results){
+  int px = blockIdx.x * blockDim.x + threadIdx.x;
+  int py = blockIdx.y * blockDim.y + threadIdx.y;
 
-  for(int px = index; px < w; px += stride){
-    for(int py = 0; py < h; py++){
-      
-      double scaledX = ((double)px - w / 2) / (w / 2) / zoom + mx;
-      double scaledY = ((double)py - h / 2) / (h / 2) / zoom + my;
+  double scaledX = ((double)px - w / 2) / (w / 2) / zoom + mx;
+  double scaledY = ((double)py - h / 2) / (h / 2) / zoom + my;
     
-      double x = 0;
-      double y = 0;
+  double x = 0;
+  double y = 0;
   
-      float it = 0;
+  double it = 0;
   
-      while (x * x + y * y < 4 && it < MAX_ITERATION)
-      {
-          double xTemp = x * x - y * y + scaledX;
-          y = 2 * x * y + scaledY;
-          x = xTemp;
+  while (x * x + y * y < 4 && it < MAX_ITERATION)
+  {
+      double xTemp = x * x - y * y + scaledX;
+      y = 2 * x * y + scaledY;
+      x = xTemp;
   
-          it++;
-      }
-
-      results[px * w + py] = it;
-    }
+      it++;
   }
+
+  results[px * w + py] = it;
 }
 
 int main(void){
-    int w = 1000;
-    int h = 1000;
+    int w = 500;
+    int h = 500;
 
-    float xpos = -1.1;
-    float ypos = 0;
+    double xpos =  -0.7463f;
+    double ypos = 0.1102f;
 
     double z = (int)1;
 
@@ -47,26 +42,33 @@ int main(void){
     {
         bitmap_image image(w, h);
 
-        float *results;
-        cudaMallocManaged(&results, w * h * sizeof(float));
+        double *results;
+        cudaMallocManaged(&results, w * h * sizeof(double));
+        std::cout << "Allocated memory" << std::endl;
 
-        int blockSize = 256;
-        int numBlocks = (w + blockSize - 1) / blockSize;
-        cudaCalc<<<numBlocks, blockSize>>>(w, h, xpos, ypos, z, results);
+        dim3 block(32,32);
+        dim3 grid;
+        grid.x = (w + block.x - 1)/block.x;
+        grid.y = (h + block.y - 1)/block.y;
+        cudaCalc<<<grid, block>>>(w, h, xpos, ypos, z, results);
+        std::cout << "Started task" << std::endl;
 
         cudaDeviceSynchronize();
 
+        std::cout << "Finished task, constructing image" << std::endl;
+
         for(int x = 0; x < w; x++){
           for (int y = 0; y < h; y++){
-            float value = results[x * w + y];
+            double value = results[x * w + y];
             image.set_pixel(x, y, (int)value, (int)value, (int)value);
           }
         }
 
-        cudaFree(results);
-
         std::cout << "Finished rendering... Saving..." << std::endl;
         image.save_image("img.bmp");
+
+        std::cout << "Finished image, freeing shared memory" << std::endl;
+        cudaFree(results);
 
         z = z * 1.1f;
     }
